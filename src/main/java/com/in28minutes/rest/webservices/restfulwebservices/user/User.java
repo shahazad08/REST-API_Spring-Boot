6465d51fdf3b1e8fdb093517d33cc113ec19dbee
shahazad08 <shaikh.shahazad@bridgelabz.com>
2024-01-14 22:27:33 +0530

    Added:Connectivity added and Entity created for user

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,19 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+@Entity(name = "user_details")
 
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2,  message = "Name should have atleast 2 characters")
     @JsonProperty("user_name")
@@ -16,9 +23,14 @@ public class User {
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
+        super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_details(id, name, birth_date) VALUES (1001, 'Shaikh', current_date());
+
+INSERT INTO user_details(id, name, birth_date) VALUES (1002, 'Azim', current_date());
+
+INSERT INTO user_details(id, name, birth_date) VALUES (1003, 'Abid', current_date());
